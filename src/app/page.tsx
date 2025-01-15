12384443ae791801087a70f19d09a7119073630b
Abhijeet <82521968+ABHIJEET-BABA@users.noreply.github.com>
2025-01-15 14:50:26 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
-import { LogoTicker } from "@/sections/LogoTicker";
+
 import { Features } from "@/sections/Features";
-import { Testimonials } from "@/sections/Testimonials";
+
 import { CallToAction } from "@/sections/CallToAction";
 import { Footer } from "@/sections/Footer";
 
@@ -12,9 +12,9 @@ export default function Home() {
     <>
       <Header />
       <Hero />
-      <LogoTicker />
+    
       <Features />
-      <Testimonials />
+     
       <CallToAction />
       <Footer/>
      
